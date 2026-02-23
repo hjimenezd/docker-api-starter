ffa8dac384cb82088eb33d9f6621652704f23fde
hjimenezd <h.jimenezd@uniandes.edu.co>
2026-02-23 18:15:20 -0500

    taller docker api starter

---
+++ bankchurn-api/app/schemas/__init__.py
@@ -0,0 +1,9 @@
+from .health import Health as Health
+from .predict import MultipleDataInputs as MultipleDataInputs
+from .predict import PredictionResults as PredictionResults
+
+__all__ = [
+    "Health",
+    "MultipleDataInputs",
+    "PredictionResults",
+]
